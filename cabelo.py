d977e30b761a341ec9e175b3e1df720ca465ad94
Luccas Garcia <luccasga@hotmail.com>
2024-09-24 23:03:16 -0300

    Testando condição

---
+++ cabelo.py
@@ -0,0 +1,10 @@
+cabelo = 35
+
+if cabelo <= 20:
+    print('Valor R$ 50,00')
+elif cabelo >= 21 and cabelo <= 30:
+    print('Valor R$ 70,00')
+elif cabelo >= 31 and cabelo <= 50:
+    print('Valor R$ 100,00')
+else:
+    print('Favor consultar a recepção')
