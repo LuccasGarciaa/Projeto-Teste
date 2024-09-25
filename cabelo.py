43b8b3740a52e925f151eb1ffcb27c9ae4572ffd
Luccas Garcia <luccasga@hotmail.com>
2024-09-25 20:33:50 -0300

    aprendendo loop

--- cabelo.py
+++ cabelo.py
@@ -8,3 +8,12 @@ elif cabelo >= 31 and cabelo <= 50:
     print('Valor R$ 100,00')
 else:
     print('Favor consultar a recepção')
+
+#Inteiro
+Velocidade = int(input('Sua Velocidade? '))
+print('Você foi multado') if Velocidade > 100 else  print('Seguir em Frente')
+
+
+#Booleano
+Velocidade_carro = False
+print('seguir em frente') if Velocidade_carro else print('Foi Multado')
---
+++ loop.py
@@ -0,0 +1,6 @@
+for numero in range(18, 111):
+  print(f'Estamos em {numero}')
+
+
+for passo in range(1, 11):
+  print(f'Realizando passo {passo}')
